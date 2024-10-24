3590cfde1d6acdf949904f7f85fb236f85452dc5
TheGreatSkyline <joaquingomez@my.whccd.edu>
2024-10-23 19:05:41 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//
+/*
+*
+*
+*/
+console.log("hello world!");
+console.log(8*null); 
+console.log('"5" - 1 =', "5" - 1); 
+console.log('"5" + 1 =', "5" + 1); 
+console.log('"five" * 2 =', "five" * 2); 
+console.log("false == 0 =", false == 0); 
+console.log("5 > 2 && 6 > 3 =", 5 > 2 && 6 > 3); 
+console.log('false == false ? "equal" : "not equal" =', false == false ? "equal" : "not equal"); 
+console.log("true || false =", true || false); /* Displays operation data into the browser console */
